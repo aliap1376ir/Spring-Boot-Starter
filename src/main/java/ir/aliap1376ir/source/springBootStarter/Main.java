275c499c2ceefb5981bfade58cb9936d7b9eec7d
Ali Azizi Pour <aliap1376ir@gmail.com>
2021-04-18 17:58:57 +0430

    day1

---
+++ src/main/java/ir/aliap1376ir/source/springBootStarter/Main.java
@@ -0,0 +1,12 @@
+package ir.aliap1376ir.source.springBootStarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
